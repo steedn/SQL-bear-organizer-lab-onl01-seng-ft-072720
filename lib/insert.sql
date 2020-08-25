6dad27d443c1e9fcf631b645c73e2a61cb205c17
Nathan Steed <steedn@outlook.com>
2020-08-25 03:29:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,7 +5,7 @@ INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0);
 INSERT INTO bears VALUES (6, "Melissa", 14, "female", "brown", "curious", 1);
 INSERT INTO bears VALUES (7, "Grinch", 66, "male", "pale brown", "very grumpy", 0);
 INSERT INTO bears VALUES (8, "Wendy", 23 "female", "pale brown", "unbothered" 1);
-INSERT INTO bears VALUES (9, NILL, 52, "female", "white", "annoyed", 1);
+INSERT INTO bears VALUES (9, NULL, 52, "female", "white", "annoyed", 1);
 
 
 -- INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
