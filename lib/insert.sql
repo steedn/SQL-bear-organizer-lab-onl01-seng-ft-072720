2d642d2cec596d3861cd7b0fc06779868036f456
Nathan Steed <steedn@outlook.com>
2020-08-25 03:30:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,10 +2,10 @@ INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
 INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 1);
 INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0);
-INSERT INTO bears VALUES (6, "Melissa", 14, "female", "brown", "curious", 1);
-INSERT INTO bears VALUES (7, "Grinch", 66, "male", "pale brown", "very grumpy", 0);
-INSERT INTO bears VALUES (8, "Wendy", 23 "female", "pale brown", "unbothered" 1);
-INSERT INTO bears VALUES (9, NULL, 52, "female", "white", "annoyed", 1);
+INSERT INTO bears VALUES (6, "Melissa", 14, "F", "brown", "curious", 1);
+INSERT INTO bears VALUES (7, "Grinch", 66, "M", "pale brown", "very grumpy", 0);
+INSERT INTO bears VALUES (8, "Wendy", 23 "F", "pale brown", "unbothered" 1);
+INSERT INTO bears VALUES (9, NULL, 52, "F", "white", "annoyed", 1);
 
 
 -- INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
