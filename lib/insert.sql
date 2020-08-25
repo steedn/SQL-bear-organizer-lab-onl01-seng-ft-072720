75d261dd1cec29373dad837cfff0c9402d6e2773
Nathan Steed <steedn@outlook.com>
2020-08-25 03:24:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "male", "brown", "friendly", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "spotted", "curious", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 45, "female", "black", "grumpy" 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 36, "male", "brown", "aggressive" 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 14, "female", "brown", "curious", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 66, "male", "pale brown", "very grumpy", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 23 "female", "pale brown", "unbothered" 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NILL, 52, "female", "white", "annoyed", 1);
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 23, "male", "brown", "friendly", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 3, "male", "spotted", "curious", 1);
+INSERT INTO bears VALUES (4, "Tabitha", 45, "female", "black", "grumpy" 0);
+INSERT INTO bears VALUES (5, "Sergeant Brown", 36, "male", "brown", "aggressive" 0);
+INSERT INTO bears VALUES (6, "Melissa", 14, "female", "brown", "curious", 1);
+INSERT INTO bears VALUES (7, "Grinch", 66, "male", "pale brown", "very grumpy", 0);
+INSERT INTO bears VALUES (8, "Wendy", 23 "female", "pale brown", "unbothered" 1);
+INSERT INTO bears VALUES (9, NILL, 52, "female", "white", "annoyed", 1);
